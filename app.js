0abad7e3548f946ad263076686fdf1fb6af5f3fd
Aadil Raza <aadil18@navgurukul.org>
2021-05-01 02:44:24 +0530

    make some change

--- app.js
+++ app.js
@@ -13,19 +13,18 @@ app.use('/fb', usersRouter);
 app.use('/fb', requestRouter);
 
 // catch 404 and forward to error handler
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 404).json({
-//     message: "No such route exists"
-//   })
-// });
+app.use(function(err, req, res, next) {
+  res.status(err.status || 404).json({
+    message: "No such route exists"
+  })
+});
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500).json({
-//     message: "Error Message"
-//   })
-// });
-
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500).json({
+    message: "Error Message"
+  })
+});
 
 // server running
 app.listen(process.env.PORT || 3000, () => {
